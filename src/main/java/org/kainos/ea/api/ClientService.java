2a55cde41d2f5e36eaae453b00e2dea1fb0888e6
Cormac Feeney <cormacfeeney@c02fl0fzml85.client.kainos.com>
2023-10-12 10:08:10 +0100

    Created necessary classes

--- src/main/java/org/kainos/ea/api/ClientService.java
+++ src/main/java/org/kainos/ea/api/ClientService.java
@@ -1,4 +1,17 @@
 package org.kainos.ea.api;
 
+import io.dropwizard.cli.Cli;
+import org.kainos.ea.cli.Client;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ClientService {
+    public List<Client> getAllClients() {
+        List<Client> clientList = new ArrayList<>();
+
+        Client client1 
+
+        return clientList;
+    }
 }
